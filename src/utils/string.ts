94d58052ad724616b0afe0d410512ea18d442bcc
Esteban Rodríguez <erodriguez@vesti.cl>
2021-05-06 23:42:46 -0400

    type for x

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,1 +1,2 @@
-export const numberWithDots = (x) => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+export const numberWithDots = (x: string) =>
+  x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
