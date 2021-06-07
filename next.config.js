df310d465181a4e293048da872d54e64d9368f75
Esteban Rodríguez <erodriguez@vesti.cl>
2021-06-07 09:48:02 -0400

    next config runtume

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 const withImages = require("next-images");
 const withPlugins = require("next-compose-plugins");
 const nextTranslate = require("next-translate");
-module.exports = withPlugins([withImages], nextTranslate());
+
+const config = {
+  serverRuntimeConfig: {
+    NEXT_PUBLIC_APP_API_URL:
+      "https://nest-generic-backend-xlqdbkwfma-ue.a.run.app/graphql",
+  },
+};
+
+module.exports = withPlugins([withImages], nextTranslate(config));
