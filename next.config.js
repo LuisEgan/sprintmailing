121056ba2c2cb04379467eb89f4d512515e3c8a9
Esteban Rodríguez <erodriguez@vesti.cl>
2021-06-07 13:38:12 -0400

    try runtime config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 const withImages = require("next-images");
 const withPlugins = require("next-compose-plugins");
 const nextTranslate = require("next-translate");
-module.exports = withPlugins([withImages], nextTranslate());
+const config = {
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_APP_API_URL: process.env.NEXT_PUBLIC_APP_API_URL,
+  },
+};
+module.exports = withPlugins([withImages], nextTranslate(config));
--- src/settings/apollo.ts
+++ src/settings/apollo.ts
@@ -1,14 +1,16 @@
 import { createHttpLink, ApolloClient, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import { onError } from "@apollo/client/link/error";
+import getConfig from "next/config";
 import { USER_TOKEN_PERSIST } from "./constants";
 
+const { publicRuntimeConfig } = getConfig();
+
 const httpLink = createHttpLink({
-  uri: process.env.NEXT_PUBLIC_APP_API_URL,
+  uri: publicRuntimeConfig.NEXT_PUBLIC_APP_API_URL,
 });
 
 // eslint-disable-next-line
-console.log("🚀 ~ file: apollo.ts ~ line 9 ~ process.env", process.env);
 
 const authLink = setContext((_, params) => {
   const { headers } = params;
