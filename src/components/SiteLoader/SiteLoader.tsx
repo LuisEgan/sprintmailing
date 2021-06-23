26928b7bbe86f1765125868595224b2cae311b38
Esteban Rodríguez <erodriguez@vesti.cl>
2021-06-23 01:32:06 -0400

    change to site-loader.json

--- src/components/SiteLoader/SiteLoader.tsx
+++ src/components/SiteLoader/SiteLoader.tsx
@@ -5,7 +5,7 @@ import React, { useContext, useEffect, useState } from "react";
 import Lottie from "react-lottie";
 import { PUBLIC_ROUTES } from "routes/routes";
 import { AFTER_LOGIN_REDIRECT } from "settings/constants";
-import lottieSiteLoader from "utils/lottie/siteLoader.json";
+import lottieSiteLoader from "utils/lottie/site-loader.json";
 
 const loaderLottieOptions = {
   loop: true,
