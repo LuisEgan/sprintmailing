20dcaea691c4b7294768813997c9132651c122b4
Esteban Rodríguez <erodriguez@vesti.cl>
2021-08-05 00:43:22 -0400

    dsimple-import-sortev deps

--- src/components/User/UserAvatar.tsx
+++ src/components/User/UserAvatar.tsx
@@ -1,4 +1,3 @@
-import { useProfile } from "context/profile/profile.context";
 import React from "react";
 import { DEFAULT_PROFILE_IMAGE } from "settings/constants";
 import { IUser } from "types/User.types";
@@ -9,54 +8,52 @@ export interface IUserAvatarProps {
   size?: number;
 }
 
-const UserAvatar = ({ user, showName, size = 50 }: IUserAvatarProps) => {
-  return (
-    <div className="flex items-center" style={{ width: size, height: size }}>
-      {user?.profileImage && user?.profileImage !== DEFAULT_PROFILE_IMAGE ? (
-        <>
+const UserAvatar = ({ user, showName, size = 50 }: IUserAvatarProps) => (
+  <div className="flex items-center" style={{ width: size, height: size }}>
+    {user?.profileImage && user?.profileImage !== DEFAULT_PROFILE_IMAGE ? (
+      <>
+        <div
+          className=" mr-2 bg-center bg-cover rounded-full border-2 border-black dark:border-white bg-gray-900 dark:bg-white"
+          style={{ width: size, height: size }}
+        >
+          <img
+            src={user?.profileImage}
+            alt="No Result"
+            width="230px"
+            className="rounded-full"
+          />
+        </div>
+        {showName && (
+          <p className="capitalize">
+            {user?.name} {user?.lastname}
+          </p>
+        )}
+      </>
+    ) : (
+      <>
+        <div className="relative mr-4" style={{ width: size, height: size }}>
           <div
-            className=" mr-2 bg-center bg-cover rounded-full border-2 border-black dark:border-white bg-gray-900 dark:bg-white"
+            className="absolute inset-0 bg-cover z-0 bg-purple-900  mr-2 bg-center rounded-full border-2 border-current-500"
             style={{ width: size, height: size }}
           >
-            <img
-              src={user?.profileImage}
-              alt="No Result"
-              width="230px"
-              className="rounded-full"
-            />
-          </div>
-          {showName && (
-            <p className="capitalize">
-              {user?.name} {user?.lastname}
-            </p>
-          )}
-        </>
-      ) : (
-        <>
-          <div className="relative mr-4" style={{ width: size, height: size }}>
             <div
-              className="absolute inset-0 bg-cover z-0 bg-purple-900  mr-2 bg-center rounded-full border-2 border-current-500"
-              style={{ width: size, height: size }}
+              className="absolute inset-0 flex justify-center items-center font-semibold text-md"
+              style={{ fontSize: size * 0.32 }}
             >
-              <div
-                className="absolute inset-0 flex justify-center items-center font-semibold text-md"
-                style={{ fontSize: size * 0.32 }}
-              >
-                {`${user?.name?.charAt(0) || ""}${
-                  user?.lastname?.charAt(0) || ""
-                }`}
-              </div>
+              {`${user?.name?.charAt(0) || ""}${
+                user?.lastname?.charAt(0) || ""
+              }`}
             </div>
           </div>
-          {showName && (
-            <p className="capitalize">
-              {user?.name} {user?.lastname}
-            </p>
-          )}
-        </>
-      )}
-    </div>
-  );
-};
+        </div>
+        {showName && (
+          <p className="capitalize">
+            {user?.name} {user?.lastname}
+          </p>
+        )}
+      </>
+    )}
+  </div>
+);
 
 export default UserAvatar;
